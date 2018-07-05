a3481bf7a2c6121f79d98482a5d4fae1d672c830
Long Gong <35161709+saber-dragon@users.noreply.github.com>
2018-07-04 19:52:02 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -8,7 +8,7 @@ git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/djui/alias-tips.git ${ZSH_CUSTOM:-$ZSH/custom}/plugins/alias-tips
-git clone git@github.com:robertzk/send.zsh.git ${ZSH_CUSTOM:-$ZSH/custom}/plugins/send
+git clone https://github.com/robertzk/send.zsh.git ${ZSH_CUSTOM:-$ZSH/custom}/plugins/send
 
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
